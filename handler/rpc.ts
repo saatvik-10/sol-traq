4e0662b1b5db0ddb5910da83e3d8b0801fd3b66f
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-10 20:16:05 +0530

    added rpc handler

---
+++ handler/rpc.ts
@@ -0,0 +1,14 @@
+const RPC = 'https://api.mainnet-beta.solana.com';
+
+const rpc = async (method: string, params: any[]) => {
+  const res = await fetch(RPC, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ jsonrpc: '2.0', id: 1, method, params }),
+  });
+  const json = await res.json();
+  if (json.error) throw new Error(json.error.message);
+  return json.result;
+};
+
+export { rpc };
